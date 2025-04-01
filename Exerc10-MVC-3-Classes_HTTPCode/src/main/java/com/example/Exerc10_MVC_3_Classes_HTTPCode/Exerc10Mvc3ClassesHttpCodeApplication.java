1246422674dea065db3aafc47908c3a3a56db27f
alexsant22 <alexandre.santana2201@gmail.com>
2025-04-01 14:39:24 -0300

    Projeto criado

---
+++ Exerc10-MVC-3-Classes_HTTPCode/src/main/java/com/example/Exerc10_MVC_3_Classes_HTTPCode/Exerc10Mvc3ClassesHttpCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Exerc10_MVC_3_Classes_HTTPCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exerc10Mvc3ClassesHttpCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exerc10Mvc3ClassesHttpCodeApplication.class, args);
+	}
+
+}
---
+++ Exerc10-MVC-3-Classes_HTTPCode/src/test/java/com/example/Exerc10_MVC_3_Classes_HTTPCode/Exerc10Mvc3ClassesHttpCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exerc10_MVC_3_Classes_HTTPCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exerc10Mvc3ClassesHttpCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
